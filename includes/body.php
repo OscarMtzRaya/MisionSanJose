46a82b63a7fa6d21ee13f0159ab1cd00c7f30984
OscarMtzRaya <ozcarr753@gmail.com>
2024-01-28 21:06:18 -0700

    Initial php commit

---
+++ includes/body.php
@@ -0,0 +1,11 @@
+<body class="body counter-scroll">
+
+    <!-- preload -->
+    <div class="preload preload-container">
+        <div class="middle"></div>
+    </div>
+    <!-- /preload -->
+
+    <div id="wrapper">
+        <!-- #page -->
+        <div id="page" class="">
---
+++ includes/scripts.php
@@ -0,0 +1,33 @@
+        </div>
+        <!-- /#page -->
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- cusor -->
+    <div class="tf-mouse tf-mouse-outer"></div>
+    <div class="tf-mouse tf-mouse-inner"></div>
+
+    <!-- go top button -->
+    <div class="progress-wrap active-progress">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewbox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 286.138;"></path>
+        </svg>
+    </div>
+
+    <!-- Javascript -->
+    <script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/swiper.js"></script>
+    <script src="assets/js/map.min.js"></script>
+    <script src="assets/js/map.js"></script>
+    <script src="assets/js/countto.js"></script>
+    <script src="assets/js/count-down.js"></script>
+    <script src="assets/js/nouislider.min.js"></script>
+    <script src="assets/js/magnific-popup.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
