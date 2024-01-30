4c8e46245f63992b9f6867e3fb0f337b6b0dd5ea
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-01-30 14:17:58 -0700

    Se crea contacto, falta terminar diseño

---
+++ modules/contact.php
@@ -0,0 +1,106 @@
+ <!-- banner-page -->
+ <div class="banner-page inner-page contact-page">
+     <div class="content">
+         <div class="banner-text">contact us</div>
+         <p class="t1">If you have any questions or concerns, you can contact us in the <br> following ways</p>
+     </div>
+ </div>
+ <!-- /banner-page -->
+
+ <!-- wg-map -->
+ <div class="wg-map">
+     <div class="themesflat-container">
+         <div class="row">
+             <div class="col-12">
+                 <div class="top">
+                     <div id="map"></div>
+                 </div>
+             </div>
+         </div>
+     </div>
+     <div class="bottom">
+         <div class="themesflat-container">
+             <div class="row">
+                 <div class="col-md-4">
+                     <div class="contact-item wow fadeInUp line-right">
+                         <div class="image"><img src="assets/images/item-background/call.png" alt=""></div>
+                         <div class="title">Contact Us</div>
+                         <p>
+                             <a href="tel:+521234567890">
+                             Phone: +52 123-456-7890
+                             </a><br>
+                             Email: <a href="#" class="__cf_email__">
+                                email@algo.com</a>
+                         </p>
+                     </div>
+                 </div>
+                 <div class="col-md-4">
+                     <div class="contact-item wow fadeInUp line-right" data-wow-delay="0.1s">
+                         <div class="image"><img src="assets/images/item-background/address.png" alt=""></div>
+                         <div class="title">Address here</div>
+                         <p>
+                             Andé Restaurant 767 5th Avenue, <br>
+                             Paris 10021, France
+                         </p>
+                     </div>
+                 </div>
+                 <div class="col-md-4">
+                     <div class="contact-item wow fadeInUp" data-wow-delay="0.2s">
+                         <div class="image"><img src="assets/images/item-background/time.png" alt=""></div>
+                         <div class="title">open time</div>
+                         <p>
+                             Mon - Fri : 9.00am - 22.00pm <br>
+                             Saturday : 11.00 - 23.00 Sunday : Close
+                         </p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- /wg-map -->
+
+ <!-- wg-quetion -->
+ <div class="wg-quetion">
+     <div class="themesflat-container">
+         <div class="row">
+             <div class="col-12">
+                 <div class="heading-section text-center">
+                     <div class="title wow fadeInUp">Have a question or need more information? Just drop us a line!
+                     </div>
+                     <p class="wow fadeInUp">Give us a call or drop by anytime, we endeavour to answer all enquiries
+                         within 24 hours <br> on business days.We will be happy to answer your questions.</p>
+                     <div class="divider wow fadeInUp">
+                         <div></div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-12">
+                 <form class="book-form">
+                     <div class="columns">
+                         <fieldset class="name">
+                             <input type="text" placeholder="Name*" class="" name="name" tabindex="2" value=""
+                                 aria-required="true" required="">
+                         </fieldset>
+                         <fieldset class="email">
+                             <input type="email" placeholder="Email*" class="" name="email" tabindex="2" value=""
+                                 aria-required="true" required="">
+                         </fieldset>
+                         <fieldset class="phone">
+                             <input type="text" placeholder="Phone*" class="" name="text" tabindex="2" value=""
+                                 aria-required="true" required="">
+                         </fieldset>
+                     </div>
+                     <fieldset class="message">
+                         <textarea id="message" name="message" rows="4" placeholder="Message" class="" tabindex="4"
+                             aria-required="true" required=""></textarea>
+                     </fieldset>
+                     <div class="bot">
+                         <a href="" class="button-two-line m-auto" type="submit">SEND A MESSAGE</a>
+                     </div>
+                 </form>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- end wg-quetion -->
