910e5b13908391797d502dab8ec4c0c2e45e2954
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-01-29 22:23:15 -0700

    footer texto ok

--- includes/footer.php
+++ includes/footer.php
@@ -5,13 +5,13 @@
                         <div class="col-lg-3 col-md-6 col-12">
                             <div class="logo-footer" id="logo-footer">
                                 <a href="index.html">
-                                    <img id="logo_footer" alt="" src="assets/images/logo/logo-ft-1.png" data-retina="assets/images/logo/logo-ft-1@2x.png">
+                                    <img id="logo_footer" alt="" src="assets/images/logo/logo.png" data-retina="assets/images/logo/logo.png">
                                 </a>
                             </div>  
                         </div>
                         <div class="col-lg-3 col-md-6 col-12">
                             <div class="footer-title">about us</div>
-                            <p>Drawing on their respective experiences in the hospitality industry,  the duo imagined a place celebrating.</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro itaque aperiam nulla? Reprehenderit, quae perspiciatis!</p>
                             <div class="widget-social style-big">
                                 <ul class="">
                                     <li><a href="#" class="icon-fb"></a></li>
@@ -30,12 +30,7 @@
                                 </li>
                                 <li>
                                     <p class="tf-color">Saturday - Sunday:</p>
-                                    <p>11:00am - 23:00pm</p>
-                                </li>
-                                <li>
-                                    <p class="tf-color">Holidays:</p>
-                                    <p>Close</p>
-                                </li>
+                                    <p>
                             </ul>
                         </div>
                         <div class="col-lg-3 col-md-6 col-12">
@@ -43,26 +38,23 @@
                             <ul class="list">
                                 <li>
                                     <p class="tf-color">Location:</p>
-                                    <p>82 Place Charles de Gaulle, Paris</p>
+                                    <p>San José del Cabo</p>
                                 </li>
                                 <li>
                                     <p class="tf-color">Email Address:</p>
-                                    <p><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="783a0a171b101d0c0c1d0a1d0b0c190d0a19160c381f15191114561b1715">[email&#160;protected]</a></p>
+                                    <p><a href="#" class="__cf_email__" >email@algo.com</a></p>
                                 </li>
                                 <li>
                                     <p class="tf-color">Phone & Book table:</p>
-                                    <p>+39-055-123456</p>
+                                    <p>
+                                        <a href="tel:+521234567890">+52 123 456 7890</a>
+                                    </p>
                                 </li>
                             </ul>
                         </div>
                         <div class="col-12">
-                            <div class="footer-bottom">
-                                <p>Copyright © 2023 themesflat. All Rights Reserved.</p>
-                                <ul>
-                                    <li><a href="#">PRIVACY</a></li>
-                                    <li><a href="#">TERM OF USE</a></li>
-                                    <li><a href="#">POLICY</a></li>
-                                </ul>
+                            <div class="footer-bottom justify-content-center">
+                                <p>Copyright © 2024  Mision San José del Cabo. All Rights Reserved. Design by <a href="">Mar-keting Ad Group</a></p>
                             </div>
                         </div>
                     </div>
