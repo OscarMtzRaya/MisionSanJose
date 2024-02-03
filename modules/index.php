f7c40c3bc0750aaa3f6a287186e553d04a3f1ae2
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-02-02 20:30:38 -0700

    padding a mapa

--- modules/index.php
+++ modules/index.php
@@ -414,8 +414,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-4">
-                <div class=" relative h-full">
+            <div class="col-xl-4" style="padding-top:3rem">
+                <div class="relative h-full">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.9640344807185!2d-109.69752609037937!3d23.062449561355578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af509d96c683db%3A0xba844638a5767c71!2sMisi%C3%B3n%20San%20Jos%C3%A9%20del%20Cabo!5e0!3m2!1sen!2smx!4v1706596511625!5m2!1sen!2smx"
                         width="100%" height="450" style="border:0; border-radius:30px; filter: grayscale(1);"
