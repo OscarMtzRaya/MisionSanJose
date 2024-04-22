8d1bd8f424f47bf0ebf486ba66f9611c5abab458
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-04-21 20:52:33 -0700

    se cambian credenciales

--- php/archivo-contacto.php
+++ php/archivo-contacto.php
@@ -28,19 +28,19 @@ $tipo_de_procesamiento = 0;
 
 try {
     // Configuración del servidor SMTP
-    $mail->SMTPDebug = 1;
+    $mail->SMTPDebug = 0;
     $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
+    $mail->Host       = 'mail.grupoteso.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'ivonne.mtz.manzo@gmail.com';
-    $mail->Password   = 'qcfh rdmb pkno brar';
+    $mail->Username   = 'info@grupoteso.com';
+    $mail->Password   = 'Grupoteso2024!';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port       = 587;
+    $mail->Port       = 465;
 
     // Configuración de remitente y destinatarios
-    $mail->setFrom('ivonne.mtz.manzo@gmail.com', 'Formulario de reservaciones');
-    $mail->addAddress('ivonne.mtz.manzo@gmail.com', 'Formulario de reservaciones');
-    $mail->addBCC('ivonne.mtz.manzo@gmail.com');
+    $mail->setFrom('info@grupoteso.com', 'Formulario de reservaciones');
+    $mail->addAddress('info@grupoteso.com', 'Formulario de reservaciones');
+    $mail->addBCC('info@grupoteso.com');
 
     // Contenido del correo en formato HTML
     $mail->isHTML(true);
