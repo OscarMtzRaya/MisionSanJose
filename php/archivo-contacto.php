cfd8117b37e57bfe61a02678ee26dadb5789878e
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-04-21 22:50:39 -0700

    smtp

--- php/archivo-contacto.php
+++ php/archivo-contacto.php
@@ -37,12 +37,12 @@ try {
     //Server settings
     $mail->SMTPDebug = 2;                                       //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'mail.hostinger.com';                   //Set the SMTP server to send through
+    $mail->Host       = 'smtp.hostinger.com';                   //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'info@grupoteso.com';    //SMTP username
     $mail->Password   = 'Grupoteso2024!';                        //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;                                  //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;                                  //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //MANDAMOS EL NOMBRE EN UNA VARIABLE PARA PODER AGREGARLE LAA COMILLA SIMPLE
     $slh = "Mision San Jose";
