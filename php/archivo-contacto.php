ecc3b7d573c0a90f64b125cdacb96b0d543c1862
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-04-21 21:04:36 -0700

    cambio host

--- php/archivo-contacto.php
+++ php/archivo-contacto.php
@@ -16,9 +16,9 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = 1;                                       //Enable verbose debug output
+    $mail->SMTPDebug = 2;                                       //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'mail.grupoteso.com';                   //Set the SMTP server to send through
+    $mail->Host       = 'mail.hostinger.com';                   //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'info@grupoteso.com';    //SMTP username
     $mail->Password   = 'Grupoteso2024!';                        //SMTP password
