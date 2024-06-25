dee9aaeae0ef4e8eb33a1fe8f6bd7178a5bc0511
OscarMtzRaya <ozcarr753@gmail.com>
2024-06-24 19:40:48 -0700

    Actualizado foto arco again

--- modules/es/index.php
+++ modules/es/index.php
@@ -85,7 +85,7 @@
         </div>
         <div class="center">
             <div class="image">
-                <img src="assets/images/fotos12feb/11-1.webp" alt="Mision San Jose">
+                <img src="assets/images/fotos12feb/11-2.webp" alt="Mision San Jose">
             </div>
         </div>
         <div class="right">
