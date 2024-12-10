94ea5bb6e9d1229947ff99400561c1601a3349cf
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-12-10 16:34:06 -0700

    form

--- modules/es/book.php
+++ modules/es/book.php
@@ -1,17 +1,5 @@
- <!-- banner-page -->
- <div class="banner-page inner-page contact-page">
-     <div class="content">
-         <div class="banner-text">Celebra el Año Nuevo con Estilo 🎉</div>
-         <p class="t1 color1">
-             ¡Despide el año con estilo y sabor! Reserva ahora para asegurar tu lugar en nuestra celebración especial de
-             Año Nuevo. 🌟
-         </p>
-     </div>
- </div>
- <!-- /banner-page -->
-
- <!-- wg-quetion -->
- <div class="wg-quetion">
+<!-- wg-quetion -->
+ <div class="wg-quetion" style="padding-top: 56px;">
      <div class="themesflat-container">
          <div class="row">
              <div class="col-12">
@@ -62,22 +50,3 @@
          </div>
      </div>
  </div>
-
- <section class="mapaNvo">
-     <div class="wg-quetion h-100 w-100" style="padding-top:0;">
-         <div class="themesflat-container">
-             <div class="row">
-                 <div class="col-12">
-                     <div class="top">
-                         <iframe
-                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1297.8794915968108!2d-109.69518677413483!3d23.06201750581786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af51a292fe8185%3A0x3aa0d0dbca3c12e1!2zTWlzacOzbiBE4oCZIFNhbiBKb3PDqQ!5e0!3m2!1ses-419!2smx!4v1707109877499!5m2!1ses-419!2smx"
-                             height="640" width="100%" style="border:0;filter: grayscale(1);" allowfullscreen=""
-                             loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
-
- </section>
- <!-- end wg-quetion -->
--- reservation.php
+++ reservation.php
@@ -5,8 +5,6 @@ include_once('includes/body.php');
 if (isset($_GET)) {
     include_once("php/msg.php");
 }
-include_once('modules/'.$lenguaje.'/header.php');
 include_once('modules/'.$lenguaje.'/book.php');
-include_once('modules/'.$lenguaje.'/footer.php');
 include_once('includes/scripts.php');
 ?>
