6ff5da1b8043b8cc78866b0643fbf79eb67d83ba
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-12-10 13:27:13 -0700

    códigos de tagmanager

--- includes/body.php
+++ includes/body.php
@@ -1,4 +1,8 @@
 <body class="body counter-scroll">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQLZ9DL2" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <!-- preload -->
     <div class="preload preload-container">
         <div class="middle"></div>
