4783f7845d2cffba6277a83da25f7f7329401f35
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-12-10 17:09:04 -0700

    cambio a inglés

--- modules/es/book.php
+++ modules/es/book.php
@@ -1,62 +1,62 @@
- <!-- banner-page -->
- <div class="banner-page inner-page contact-page">
-     <div class="content">
-         <div class="banner-text">Celebra el Año Nuevo con Estilo 🎉</div>
-         <p class="t1 color1">
-             ¡Despide el año con estilo y sabor! Reserva ahora para asegurar tu lugar en nuestra celebración especial de
-             Año Nuevo. 🌟
-         </p>
-     </div>
- </div>
- <!-- /banner-page -->
+  <!-- banner-page -->
+  <div class="banner-page inner-page contact-page">
+      <div class="content">
+          <div class="banner-text">Celebrate the New Year in Style 🎉</div>
+          <p class="t1 color1">
+              Say Goodbye to the Year with Style and Flavor! Reserve now to secure your spot at our special New Year’s
+              celebration. 🌟
+          </p>
+      </div>
+  </div>
+  <!-- /banner-page -->
 
- <!-- wg-quetion -->
- <div class="wg-quetion" style="padding-top: 56px;">
-     <div class="themesflat-container">
-         <div class="row justify-content-center align-items-center">
-             <div hidden class="col-12">
-                 <div class="heading-section text-center">
-                     <div class="title wow fadeInUp">
-                         Despide el Año con Buen Gusto 🍷
-                     </div>
-                     <p class="wow fadeInUp">
-                         Pre-reserva tu lugar para nuestra cena de Año Nuevo y comienza el 2025 con los mejores sabores
-                         y compañía. ¡Estamos ansiosos por celebrar contigo!
-                     </p>
-                     <div class="divider wow fadeInUp">
-                         <div></div>
-                     </div>
-                 </div>
-             </div>
-             <div class="col-12 justify-content-center">
-                 <form class="" method="POST" action="/php/book.php">
-                     <div class="row justify-content-center">
-                         <div class="col-md-8">
-                             <fieldset class="name">
-                                 <input type="text" placeholder="Nombre Completo*" class="" name="nombre" tabindex="2"
-                                     value="" aria-required="true" required="">
-                             </fieldset>
-                         </div>
-                         <div class="col-md-8">
-                             <fieldset class="email">
-                                 <input type="email" placeholder="Correo*" class="" name="correo" tabindex="2" value=""
-                                     aria-required="true" required="">
-                             </fieldset>
-                         </div>
-                         <div class="col-md-8">
-                             <fieldset class="phone">
-                                 <input type="text" placeholder="Télefono*" class="" name="tel" tabindex="2" value=""
-                                     aria-required="true" required="">
-                             </fieldset>
-                         </div>
+  <!-- wg-quetion -->
+  <div class="wg-quetion" style="padding-top: 56px;">
+      <div class="themesflat-container">
+          <div class="row justify-content-center align-items-center">
+              <div hidden class="col-12">
+                  <div class="heading-section text-center">
+                      <div class="title wow fadeInUp">
+                          Despide el Año con Buen Gusto 🍷
+                      </div>
+                      <p class="wow fadeInUp">
+                          Pre-reserva tu lugar para nuestra cena de Año Nuevo y comienza el 2025 con los mejores sabores
+                          y compañía. ¡Estamos ansiosos por celebrar contigo!
+                      </p>
+                      <div class="divider wow fadeInUp">
+                          <div></div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-12 justify-content-center">
+                  <form class="" method="POST" action="/php/book.php">
+                      <div class="row justify-content-center">
+                          <div class="col-md-8">
+                              <fieldset class="name">
+                                  <input type="text" placeholder="Full Name*" class="" name="nombre" tabindex="2"
+                                      value="" aria-required="true" required="">
+                              </fieldset>
+                          </div>
+                          <div class="col-md-8">
+                              <fieldset class="email">
+                                  <input type="email" placeholder="Email*" class="" name="correo" tabindex="2" value=""
+                                      aria-required="true" required="">
+                              </fieldset>
+                          </div>
+                          <div class="col-md-8">
+                              <fieldset class="phone">
+                                  <input type="text" placeholder="Phone number*" class="" name="tel" tabindex="2" value=""
+                                      aria-required="true" required="">
+                              </fieldset>
+                          </div>
 
-                         <div class="bot">
-                             <button href="" class="button-two-line m-auto" type="submit">PRE-RESERVACIÓN</button>
-                         </div>
-                     </div>
+                          <div class="bot">
+                              <button href="" class="button-two-line m-auto" type="submit">PRE-RESERVATION</button>
+                          </div>
+                      </div>
 
-                 </form>
-             </div>
-         </div>
-     </div>
- </div>
+                  </form>
+              </div>
+          </div>
+      </div>
+  </div>
